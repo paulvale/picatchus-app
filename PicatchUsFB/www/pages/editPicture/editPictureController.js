94005ed9bbf92c7d79e88f7b2a46398100bb0195
Lucas Willemot <willemotlucas@gmail.com>
2015-10-27 18:15:18 +0100

    fix bug to display photo in validation view each time we take one + add pictachus hashtag on photo's description

--- PicatchUsFB/www/pages/editPicture/editPictureController.js
+++ PicatchUsFB/www/pages/editPicture/editPictureController.js
@@ -1,10 +1,7 @@
 app.controller('EditPictureController', function ($scope, ngFB, $stateParams, $localstorage, $state, $cordovaFile, $cordovaFileTransfer, $cordovaToast, $timeout){
-
-
     $scope.init = function(){
-    	var imageURI = $stateParams.imageURI;
-        var photo = document.getElementById('photo');
-        photo.src = imageURI;
+        $scope.data = {}
+        $scope.data.imageURI = $stateParams.imageURI;
         if(ionic.Platform.isAndroid()){ //Not implemented for iOS yet
             $timeout(function(){
                 window.canvas2ImagePlugin.saveImageDataToLibrary(
@@ -15,7 +12,7 @@ app.controller('EditPictureController', function ($scope, ngFB, $stateParams, $l
                     },
                     imageURI
                 );
-            }, 3000);
+            }, 2000);
         }
     }
 
@@ -23,10 +20,13 @@ app.controller('EditPictureController', function ($scope, ngFB, $stateParams, $l
         var fileURI = document.getElementById('photo').src;
         var options = new FileUploadOptions();
         var params = {};
-        params.caption = $scope.description;
+        if($scope.data.description == undefined)
+            params.caption = "Pris avec <3 par #PicatchUs";
+        else
+            params.caption = $scope.data.description + " - Pris avec <3 par #PicatchUs";
         options.params = params;
 
-        $cordovaFileTransfer.upload("https://graph.facebook.com/404456883087336/photos?access_token=" + window.localStorage.fbAccessToken, fileURI, options)
+        $cordovaFileTransfer.upload("https://graph.facebook.com/404456883087336/photos?access_token=" + window.localStorage.fbAccessToken, $scope.data.imageURI, options)
           .then(function(result) {
             $cordovaToast.showLongBottom('Votre photo a bien été envoyée !');
           }, function(err) {
@@ -36,10 +36,12 @@ app.controller('EditPictureController', function ($scope, ngFB, $stateParams, $l
             // constant progress updates
           });
 
+        $scope.data.imageURI = '';
         $state.go('home.eventsFeed');
     }
 
     $scope.quit = function(){
+        $scope.data.imageURI = '';
     	$state.go('home.eventsFeed');
-    } 
+    }
 });
