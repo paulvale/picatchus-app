35a5278098f727ccffa05a18beb05e22bc8e0ac7
vpaul <valentinpaul04@gmail.com>
2015-11-02 20:55:08 +0100

    fixed bug like on eventsFeed

--- PicatchUsFB/www/pages/eventsFeed/eventsFeedController.js
+++ PicatchUsFB/www/pages/eventsFeed/eventsFeedController.js
@@ -1,4 +1,4 @@
-app.controller('EventsFeedController',function ($scope, $cordovaToast, EventsFactory, PhotoFactory){
+app.controller('EventsFeedController',function ($scope,$ionicModal, $cordovaToast, EventsFactory, PhotoFactory){
     function getPhotosLiveEvents(refresh){
     	$scope.liveEvents = EventsFactory.getPhotosLiveEvents().then(function(livePhotos){
     		$scope.livePhotos = livePhotos;
@@ -17,27 +17,52 @@ app.controller('EventsFeedController',function ($scope, $cordovaToast, EventsFac
 
     $scope.like = function(idPhoto, posPhoto){
     	console.log('like');
-        $scope.photos[posPhoto].total_likes++;
-        $scope.photos[posPhoto].has_liked = true;
+        console.log(posPhoto);
+        $scope.livePhotos[posPhoto].total_likes++;
+        $scope.livePhotos[posPhoto].has_liked = true;
         PhotoFactory.like(idPhoto).then(function(result){
         }, function(msg){
-            $scope.photos[posPhoto].total_likes--;
-            $scope.photos[posPhoto].has_liked = false;
+            $scope.livePhotos[posPhoto].total_likes--;
+            $scope.livePhotos[posPhoto].has_liked = false;
         })  
     }
 
     $scope.dislike = function(idPhoto, posPhoto){
     	console.log('dislike');
-        $scope.photos[posPhoto].total_likes--;
-        $scope.photos[posPhoto].has_liked = false;
+        $scope.livePhotos[posPhoto].total_likes--;
+        $scope.livePhotos[posPhoto].has_liked = false;
         PhotoFactory.like(idPhoto).then(function(result){
         }, function(msg){
-            $scope.photos[posPhoto].total_likes++;
-            $scope.photos[posPhoto].has_liked = true;
+            $scope.livePhotos[posPhoto].total_likes++;
+            $scope.livePhotos[posPhoto].has_liked = true;
         })
     }
 
     $scope.refresh = function (){
     	getPhotosLiveEvents(true);
     }
+
+    /* ========================================*/
+    /* =========== PHOTO MODAL ================*/
+    /* ========================================*/
+
+    $ionicModal.fromTemplateUrl('photo_modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openModal = function() {
+        $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+
 })
