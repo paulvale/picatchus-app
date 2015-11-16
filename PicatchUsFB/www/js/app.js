faa69f0a540b46897549df3353b4f355bd14eeb4
vpaul <valentinpaul04@gmail.com>
2015-11-16 23:08:27 +0100

    fixed bug hide bottom bar on keyboard open

--- PicatchUsFB/www/js/app.js
+++ PicatchUsFB/www/js/app.js
@@ -13,7 +13,7 @@ angular.module('starter', ['ionic', 'ImgCache', 'ngLocalStorage','ui.router', 'n
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
         cordova.plugins.Keyboard.disableScroll(true);
 
       }
